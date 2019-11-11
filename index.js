3bbb45dd0413f4c58416c09ffab79c8e53665a95
Leo Dutra <leodutra.br@gmail.com>
2019-11-11 13:50:34 -0300

    Change index.js formatting

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const inputFlow  = require('./core/input-flow')
-const textFlow   = require('./core/text-flow')
-const imageFlow  = require('./core/image-flow')
-const videoFlow  = require('./core/video-flow')
+const inputFlow = require('./core/input-flow')
+const textFlow = require('./core/text-flow')
+const imageFlow = require('./core/image-flow')
+const videoFlow = require('./core/video-flow')
 const uploadFlow = require('./core/upload-flow')
 
-const { State }= require('./core/state')
+const { State } = require('./core/state')
 
 async function start() {
-  try {
     const state = new State()
     await state.init({
-      credentials: {
-        watsonNlu: require('./credentials/watson-nlu.json'),
-        googleSearch: require('./credentials/google-search.json'),
-        watsonClassifier: require('./credentials/watson-classifier.json'),
-        // algorithmia: require('./credentials/algorithmia.json'),
-      }
+        credentials: {
+            watsonNlu: require('./credentials/watson-nlu.json'),
+            googleSearch: require('./credentials/google-search.json'),
+            watsonClassifier: require('./credentials/watson-classifier.json'),
+            // algorithmia: require('./credentials/algorithmia.json'),
+        }
     })
     await state.propagate(
-      inputFlow,
-      textFlow,
-      imageFlow,
-      videoFlow,
-      uploadFlow
+        inputFlow,
+        textFlow,
+        imageFlow,
+        videoFlow,
+        uploadFlow
     )
-  }
-  catch(error) {
-    console.error(error)
-  }
 }
 
-start();
+start().catch(error => {
+    console.error(error)
+    process.exit(error.code || 1)
+})

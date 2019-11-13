7393ea4a1d9112aa210b9651b6262da7499f3a01
Leo Dutra <leodutra.br@gmail.com>
2019-11-13 05:16:57 -0300

    Implements sync state save/load

--- core/state.js
+++ core/state.js
@@ -1,25 +1,22 @@
 const fs = require('fs')
-const { readFile, writeFile } = require('./utils')
 
 const defaultPath = name => `./state-${name}.json`
 
 class State {
     constructor(name = 'default') {
         this.filepath = defaultPath(name)
-        if (fs.existsSync(this.filepath)) {
-            this.load()
-        } else {
+        if (!fs.existsSync(this.filepath)) {
             this.init()
         }
     }
-    async init(initialState = {}) {
+    init(initialState = {}) {
         this.save(initialState)
     }
-    async save(content) {
-        await writeFile(this.filepath, JSON.stringify(content, null, 2))
+    save(content) {
+        fs.writeFileSync(this.filepath, JSON.stringify(content, null, 2))
     }
-    async load() {
-        return JSON.parse(await readFile(this.filepath, 'utf-8'))
+    load() {
+        return JSON.parse(fs.readFileSync(this.filepath))
     }
     async propagate(...functions) {
         for (const fn of functions) {
@@ -28,13 +25,12 @@ class State {
                     `> ${this.constructor.name}.${this.propagate.name}() requires only functions.`
                 )
             }
-            const result = await fn(await this.load())
+            const result = await fn(this.load())
             console.log(
                 `> ${this.constructor.name}: ${fn.name || 'anonymous'}() returned keys: { ${Object.keys(result).join(', ')} }`
             )
-            await this.save(result)
+            this.save(result)
         }
-        return this
     }
 }
 
